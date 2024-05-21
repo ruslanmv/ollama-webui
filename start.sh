ef241a75d7b63536ce61eb23aff02b510e0d199d
Ruslan Idelfonso Magana Vsevolodovna <ruslan.idelfonso.magana.vsevolodovna-cic@ibm.com>
2024-05-21 23:23:34 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 # Starting server
 echo "Starting server"
+#ollama serve &
+#sleep 1
+#ollama pull llama3
+#python run.py
+# Start Ollama in the background.
 ollama serve &
-sleep 1
+# Record Process ID.
+pid=$!
+# Pause for Ollama to start.
+sleep 5
+echo "🔴 Retrieve LLAMA3 model..."
 ollama pull llama3
-python run.py
+echo "🟢 Done!"
+# Wait for Ollama process to finish.
+wait $pid
+# Start the Python application
+echo "Starting Python application..."
+python ./run.py
 
