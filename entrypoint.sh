a871eb356394141d95773e85630e4b72fa6d1ff8
Ruslan Idelfonso Magana Vsevolodovna <ruslan.idelfonso.magana.vsevolodovna-cic@ibm.com>
2024-05-22 09:19:35 +0200

    some models

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,18 @@ echo "Starting Ollama server"
 ollama serve &
 sleep 1
 
+# Try to get the model environment variable
+if [ -n "${MODEL}" ]; then
+  # Split the MODEL variable into an array
+  IFS=',' read -ra MODELS <<< "${MODEL}"
+else
+  # Use the default list of models
+  MODELS=(phi3 mistral llama3 gemma:2b)
+fi
+
+
 # Splitting the models by comma and pulling each
-IFS=',' read -ra MODELS <<< "$model"
+#IFS=',' read -ra MODELS <<< "$model"
 for m in "${MODELS[@]}"; do
     echo "Pulling $m"
     ollama pull "$m"
